967bafdb4632dbe7a92dfd092ffb7e0b51ae2132
José Eduardo Soares de Lima Nogueira <eduardosoaresn3@gmail.com>
2022-09-12 12:15:32 -0300

    Segundo commit com início do projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+- New Repository by Dexduo.
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Meu Site</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h2>Bem-vindo ao meu Site</h2>
+        <div class="panel">
+            <img src="hamster-meme.gif" alt="Meme do Hamster">
+            <a href="https://www.google.com">Click me</a>
+        </div>
+    </body>
+</html>
